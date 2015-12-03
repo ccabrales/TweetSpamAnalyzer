1e070b76fe9dd6b1f88fc55e22786113146dfc37
Casey Cabrales <caseycab@stanford.edu>
2015-12-02 20:46:29 -0800

    Implementing feature extraction for combined files for use with k-folding cross validation

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ trainFiles = ['./tweets/train/gta5Train.json', './tweets/train/counterstrikeTrai
 testFiles = ['./tweets/test/gta5Test.json', './tweets/test/counterstrikeTest.json', './tweets/test/skyrimTest.json',\
 './tweets/test/rocketleagueTest.json', './tweets/test/witcher3Test.json', './tweets/test/troveTest.json']
 
+foldingFiles = ['./tweets/k-folding/counterstrike.json', './tweets/k-folding/gta5.json', './tweets/k-folding/rocketleague.json',\
+'./tweets/k-folding/skyrim.json', './tweets/k-folding/witcher3.json', './tweets/k-folding/trove.json']
+
 trainThreshold = time.strptime("Tue Aug 11 00:00:00 +0000 2015", "%a %b %d %H:%M:%S +0000 %Y")
 
 # Download tweets separated into single archive file for future data purposes
@@ -231,6 +234,53 @@ def extractFeatures():
 	return (trainResults, testResults)
 
 
+#Extract features from the files for each game, for use with folding
+def extractFeaturesFolding():
+	trainResults = {}
+	testResults = {}
+
+	playerCounts = []
+	with open('game_stats.json') as f:
+		playerCounts = json.load(f)
+
+	for index, game in enumerate(gameNames):
+		features = []
+		users = [set()] * 31 # init each day in the set to have empty set of names
+		tweetCounts = collections.Counter() # init each day in the set to have 0 tweets
+		favoriteCounts = collections.Counter() # init each day in the set to have 0 follower count
+		followerCounts = collections.Counter() # init each day in the set to have 0 follower count
+		retweetCounts = collections.Counter() # init each day in the set to have 0 retweets
+
+		countIndex = [i for i, val in enumerate(playerCounts) if val["Game"] == game][0]
+
+		with open(foldingFiles[index]) as d:
+			for line in d:
+				item = json.loads(line)
+				t = time.strptime(item['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
+				day = t.tm_mday - 1 #get day minus one for index
+
+				if item['user']['screen_name'] not in users[day]:
+					retweetCounts[day] += item['retweet_count']
+					users[day].add(item['user']['screen_name'])
+
+				tweetCounts[day] += 1 #increment tweet counts for this day
+				favoriteCounts[day] += item['favorite_count']
+				followerCounts[day] += item['user']['followers_count']
+
+		# Build the feature vector for each day
+		for i in xrange(31):
+			favAvg = float(favoriteCounts[i]) / tweetCounts[i] if tweetCounts[i] > 0 else 0
+			followAvg = float(followerCounts[i]) / len(users[i]) if len(users[i]) > 0 else 0
+			retweetAvg = float(retweetCounts[i]) / tweetCounts[i] if tweetCounts[i] > 0 else 0
+			feat = [tweetCounts[i], len(users[i]), favAvg, followAvg, retweetAvg]
+			formatDate = '08/' + (('0' + str(i+1)) if i+1 < 10 else str(i+1)) + '/15'
+			features.append((feat, playerCounts[countIndex]["Peak Player"][formatDate]))
+
+		#take only the days that we are currently taking a look at
+		trainResults[game] = features[:10]
+		testResults[game] = features[10:]
+
+	return (trainResults, testResults)
 
 
 if __name__ == "__main__":
